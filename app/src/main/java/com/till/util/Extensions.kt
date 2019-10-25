7092284d3803e60904a5aed40ec0893a0f46c7ef
James Gliwa <jgliwa12@gmail.com>
2019-10-24 18:24:14 -0700

    removing log from extension function

--- app/src/main/java/com/till/util/Extensions.kt
+++ app/src/main/java/com/till/util/Extensions.kt
@@ -1,11 +1,8 @@
 package com.till.util
 
-import timber.log.Timber
-
 fun <K, V> MutableMap<K, V>.mergeReduceInPlace(vararg otherMap: Map<K, V>, reduce: (V, V) -> V) =
     otherMap.forEach { otherEntry ->
         otherEntry.forEach {
-            Timber.v("Merging [${it.key}, ${it.value}]...")
             merge(it.key, it.value, reduce)
         }
     }
