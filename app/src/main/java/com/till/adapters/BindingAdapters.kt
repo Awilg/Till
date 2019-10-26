6337dabadd493e9f19f292b1cc19bad40727d90f
James Gliwa <jgliwa12@gmail.com>
2019-10-26 11:43:56 -0700

    loading profile images in

--- app/src/main/java/com/till/adapters/BindingAdapters.kt
+++ app/src/main/java/com/till/adapters/BindingAdapters.kt
@@ -1,7 +1,9 @@
 package com.till.adapters
 
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -10,4 +12,14 @@ var format = SimpleDateFormat("MM/dd/yyyy", Locale.US)
 @BindingAdapter("formatTimestamp")
 fun formatTimestamp(view: TextView, timestamp: String) {
     view.text = format.format(Date(timestamp.toLong()))
+}
+
+@BindingAdapter("imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view.context)
+            .load(imageUrl)
+            .dontAnimate()
+            .into(view)
+    }
 }
--- app/src/main/java/com/till/ui/main/MainViewModel.kt
+++ app/src/main/java/com/till/ui/main/MainViewModel.kt
@@ -1,8 +1,6 @@
 package com.till.ui.main
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.till.data.Connection
 import com.till.data.ConnectionRepository
@@ -12,12 +10,8 @@ class MainViewModel internal constructor(
     private val connectionRepository: ConnectionRepository
 ) : ViewModel() {
 
-    private val _connections = MutableLiveData<List<Connection>>()
     val connections: LiveData<List<Connection>>
-        get() = _connections
-
-
-    private val connectionList = MediatorLiveData<List<Connection>>()
+        get() = connectionRepository.getConnections()
 
     init {
         Timber.i("MainViewModel created...")
