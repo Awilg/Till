a0f2c1c3849a0d9abce1b4f6a00ed6a1305c0b14
James Gliwa <jgliwa12@gmail.com>
2019-11-17 15:13:47 -0800

    dynamically set the dark mode setting to reflect current config

--- app/src/main/java/com/till/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/till/ui/settings/SettingsFragment.kt
@@ -5,25 +5,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
-import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.till.R
-
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.user_preferences, rootKey)
 
-        val darkMode: Preference? = findPreference(getString(R.string.pref_dark_mode))
+        val darkMode: SwitchPreference? = findPreference(getString(R.string.pref_dark_mode))
 
         val config = resources.configuration
         when (config.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
             Configuration.UI_MODE_NIGHT_NO -> {
-                darkMode?.setDefaultValue(false)
+                darkMode?.isChecked = false
             } // Night mode is not active, we're using the light theme
             Configuration.UI_MODE_NIGHT_YES -> {
-                darkMode?.setDefaultValue(true)
+                darkMode?.isChecked = true
             } // Night mode is active, we're using dark theme
         }
 
