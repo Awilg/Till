975992d697a1f8e159a1c7ec6172bd3b16ee72c5
James Gliwa <jgliwa12@gmail.com>
2019-11-16 16:46:45 -0800

    back to 1/day

--- app/src/main/java/com/till/ui/main/MainFragment.kt
+++ app/src/main/java/com/till/ui/main/MainFragment.kt
@@ -85,8 +85,8 @@ class MainFragment : Fragment(), EasyPermissions.PermissionCallbacks {
                         if (list.isNullOrEmpty()) {
                             // If we can't find a job already running we create one
                             val request = PeriodicWorkRequestBuilder<PushNotificationWorker>(
-                                15,
-                                TimeUnit.MINUTES
+                                1,
+                                TimeUnit.DAYS
                             )
                                 .addTag("scheduled-push")
                                 .setConstraints(
