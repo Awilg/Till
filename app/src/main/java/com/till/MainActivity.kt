6f678981a6279b525e894749392c40f61c30a48a
James Gliwa <jgliwa12@gmail.com>
2019-11-17 14:18:53 -0800

    status bar dark mode fix

--- app/src/main/java/com/till/MainActivity.kt
+++ app/src/main/java/com/till/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.till
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.findNavController
@@ -25,12 +24,6 @@ class MainActivity : AppCompatActivity() {
             NotificationManagerCompat.IMPORTANCE_DEFAULT, false,
             getString(R.string.app_name), "App notification channel."
         )
-
-        // draw behind status and nav bar
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-        //View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
         setUpNavigation()
     }
