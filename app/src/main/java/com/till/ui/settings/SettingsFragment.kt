b1c6738dacbb1be39ade7287a9b2176918214c7c
James Gliwa <jgliwa12@gmail.com>
2019-11-17 14:46:27 -0800

    toggle dark mode setting

--- app/src/main/java/com/till/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/till/ui/settings/SettingsFragment.kt
@@ -1,11 +1,40 @@
 package com.till.ui.settings
 
+import android.content.res.Configuration
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.till.R
 
+
 class SettingsFragment : PreferenceFragmentCompat() {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.user_preferences, rootKey)
+
+        val darkMode: Preference? = findPreference(getString(R.string.pref_dark_mode))
+
+        val config = resources.configuration
+        when (config.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_NO -> {
+                darkMode?.setDefaultValue(false)
+            } // Night mode is not active, we're using the light theme
+            Configuration.UI_MODE_NIGHT_YES -> {
+                darkMode?.setDefaultValue(true)
+            } // Night mode is active, we're using dark theme
+        }
+
+        darkMode?.setOnPreferenceChangeListener { _, newValue ->
+            if (newValue as Boolean) {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO)
+            }
+            true
+        }
+
     }
 }
