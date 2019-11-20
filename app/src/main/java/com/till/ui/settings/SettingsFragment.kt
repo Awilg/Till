a22eae5e3d5571f64ba1d6eaff96a1441965b831
James Gliwa <jgliwa12@gmail.com>
2019-11-19 21:38:59 -0800

    rate app preference implemented

--- app/src/main/java/com/till/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/till/ui/settings/SettingsFragment.kt
@@ -1,10 +1,14 @@
 package com.till.ui.settings
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 import com.till.R
@@ -15,6 +19,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setPreferencesFromResource(R.xml.user_preferences, rootKey)
 
         val darkMode: SwitchPreference? = findPreference(getString(R.string.pref_dark_mode))
+        val rateAppStore: Preference? = findPreference(getString(R.string.pref_rate_app))
 
         val config = resources.configuration
         when (config.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
@@ -35,5 +40,29 @@ class SettingsFragment : PreferenceFragmentCompat() {
             true
         }
 
+        rateAppStore?.setOnPreferenceClickListener {
+
+
+            val uri = Uri.parse("market://details?id=" + context?.packageName)
+            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+            // To count with Play market backstack, After pressing back button,
+            // to taken back to our application, we need to add following flags to intent.
+            goToMarket.addFlags(
+                Intent.FLAG_ACTIVITY_NO_HISTORY or
+                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK
+            )
+            try {
+                startActivity(goToMarket)
+            } catch (e: ActivityNotFoundException) {
+                startActivity(
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=" + context?.packageName)
+                    )
+                )
+            }
+            true
+        }
     }
 }
